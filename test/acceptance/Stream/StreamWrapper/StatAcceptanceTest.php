c3ac67db8a45076f3960392e3625659a6e1b35a6
adlawson <adlawson@gmail.com>
2014-09-06 14:25:54 +0100

    Add `is_dir` and `is_file` acceptance tests

---
+++ test/acceptance/Stream/StreamWrapper/StatAcceptanceTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Vfs\Stream\StreamWrapper;
+
+use Vfs\Test\AcceptanceTestCase;
+
+class StatAcceptanceTest extends AcceptanceTestCase
+{
+    protected $tree = [
+        'foo' => [
+            'bar' => 'baz'
+        ]
+    ];
+
+    public function testIsDir()
+    {
+        $this->assertTrue(is_dir("$this->scheme://foo"));
+    }
+
+    public function testIsFile()
+    {
+        $this->assertTrue(is_file("$this->scheme://foo/bar"));
+    }
+}
