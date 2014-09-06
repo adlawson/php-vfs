b8d8ddf714826fcacf2cd4d6ca66e84683e76f11
adlawson <adlawson@gmail.com>
2014-09-06 14:26:22 +0100

    Add `require` and `include` acceptance tests

---
+++ test/acceptance/Stream/StreamWrapper/RequireAcceptanceTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Vfs\Stream\StreamWrapper;
+
+use Vfs\Test\AcceptanceTestCase;
+
+/**
+ * @todo testRequireMissingFile correctly triggers fatal error, not easy to test
+ */
+class RequireAcceptanceTest extends AcceptanceTestCase
+{
+    protected $tree = [
+        'foo' => [
+            'bar.php' => '<?php return "baz";'
+        ]
+    ];
+
+    public function testIncludeFile()
+    {
+        $this->assertEquals('baz', include "$this->scheme://foo/bar.php");
+    }
+
+    public function testRequireFile()
+    {
+        $this->assertEquals('baz', require "$this->scheme://foo/bar.php");
+    }
+
+    public function testIncludeMissingFile()
+    {
+        $this->setExpectedException('PHPUnit_Framework_Error_Warning');
+
+        include "$this->scheme://bar.php";
+    }
+}
