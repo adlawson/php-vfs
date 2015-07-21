4a0dd8dc62c610cfdde981ea06e8ff88151ea8a3
adlawson <adlawson@gmail.com>
2015-07-21 15:17:00 +0100

    Update license year to 2015
    ...eventually. Not long til 2016.

--- test/src/AcceptanceTestCase.php
+++ test/src/AcceptanceTestCase.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of VFS
  *
- * Copyright (c) 2014 Andrew Lawson <http://adlawson.com>
+ * Copyright (c) 2015 Andrew Lawson <http://adlawson.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
